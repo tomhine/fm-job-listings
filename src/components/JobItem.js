9c3212e67c5c818746162b73556f2a1ba33272a2
tomhine792 <tomhine792@outlook.com>
2021-07-29 22:37:56 +0100

    added styling to company name and info pills

--- src/components/JobItem.js
+++ src/components/JobItem.js
@@ -6,7 +6,8 @@ const Item = styled.div`
   align-items: center;
   background-color: white;
   border-radius: 5px;
-  border-left: 5px solid white;
+  border-left: 5px solid
+    ${({ featured }) => (featured ? 'var(--primary)' : 'white')};
   width: 327px;
   height: 258px;
 `;
@@ -14,13 +15,54 @@ const Item = styled.div`
 const Info = styled.div`
   display: flex;
   flex-direction: column;
+  /* justify-content: flex-start; */
   padding: 10px;
+  width: 95%;
 `;
+
+const Row = styled.span`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  min-width: 100%;
+`;
+
+const Company = styled.h3`
+  color: var(--primary);
+  font-size: 14px;
+  margin-right: 10px;
+`;
+
+const NewPill = styled.div`
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
+  padding: 7px 8px 4px 8px;
+  text-transform: uppercase;
+  background-color: var(--primary);
+  color: white;
+  border-radius: 20px;
+  font-weight: 700;
+  font-size: 11px;
+  text-align: center;
+`;
+
+const FeaturedPill = styled(NewPill)`
+  background-color: var(--dark-text);
+`;
+
+const Position = styled.h1``;
 
 const JobItem = ({ data }) => {
   return (
-    <Item>
-      <Info>{data.company}</Info>
+    <Item featured={data.featured}>
+      <Info>
+        <Row>
+          <Company>{data.company}</Company>
+          {data.new && <NewPill>NEW!</NewPill>}
+          {data.featured && <FeaturedPill>FEATURED</FeaturedPill>}
+        </Row>
+      </Info>
     </Item>
   );
 };
