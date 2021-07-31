e1863ec6740529d55c87fc9ea3a61f5a25d9b8f2
Tom Hine <tomhine792@gmail.com>
2021-07-31 20:52:56 +0100

    fixed filtering, each item must have all tags

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import JobList from './JobList';
 import dataList from '../data/data.json';
+import Filters from './Filters';
 
 const MainContainer = styled.main`
   display: flex;
@@ -15,19 +16,38 @@ const Main = () => {
   const [filters, setFilters] = useState([]);
 
   const addFilterHandler = e => {
+    if (filters.includes(e.target.innerText)) return;
     setFilters(cur => [...cur, e.target.innerText]);
-    console.log(filters);
   };
 
-  const filteredData = dataList.filter(
-    data =>
-      filters.includes(data.role) ||
-      filters.includes(data.level) ||
-      filters.includes(...data.languages)
-  );
+  const filteredData = dataList.filter(data => {
+    const dataInfo = [data.role, data.level, ...data.languages, ...data.tools];
+    return filters.every(filter => dataInfo.includes(filter));
+  });
+
+  const clearAllFiltersHandler = () => {
+    setFilters([]);
+  };
+
+  const removeFilterHandler = (e, item) => {
+    const index = filters.indexOf(item);
+    if (index < 0) return;
+    setFilters(currentFilters => {
+      const newFilters = [...currentFilters];
+      newFilters.splice(index, 1);
+      return newFilters;
+    });
+  };
 
   return (
     <MainContainer>
+      {filters.length > 0 && (
+        <Filters
+          filters={filters}
+          onClearAllFilters={clearAllFiltersHandler}
+          onRemoveFilter={removeFilterHandler}
+        />
+      )}
       <JobList
         dataList={filters.length < 1 ? dataList : filteredData}
         onAddFilter={addFilterHandler}
